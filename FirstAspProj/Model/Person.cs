6881d7f5a5ec5b05470a0b44b7296b800579e521
ProdunVladyslav <produn.vlad@gmail.com>
2025-11-10 22:31:42 +0200

    first commit

---
+++ FirstAspProj/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorEx.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Profession { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public List<Skills> Skills { get; set; } = [];
+    }
+}
---
+++ FirstAspProj/Model/Skills.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorEx.Models
+{
+    public class Skills
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Proficiency { get; set; }
+    }
+}
